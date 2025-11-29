b592ad90b7d6135f69443e000529994a077461bb
Naufal Fadhil <naufalfadhil12344@gmail.com>
2025-11-29 17:30:01 +0700

    [Feature-Auth] Create Auth Repo Implementation

---
+++ lib/features/auth/data/repositories/auth_repo_impl.dart
@@ -0,0 +1,43 @@
+import 'package:electrum/core/session_handler/session.dart';
+import 'package:electrum/core/types/nothing.dart';
+import 'package:electrum/core/types/repo_result.dart';
+import 'package:electrum/features/auth/data/datasources/auth_local_dts.dart';
+import 'package:electrum/features/auth/data/datasources/auth_network_dts.dart';
+import 'package:electrum/features/auth/domain/entities/login_form.dart';
+import 'package:electrum/features/auth/domain/entities/register_form.dart';
+import 'package:electrum/features/auth/domain/entities/user.dart';
+import 'package:electrum/features/auth/domain/repositories/auth_repo.dart';
+import 'package:injectable/injectable.dart';
+
+@Injectable(as: AuthRepo)
+class AuthRepoImpl implements AuthRepo {
+  final AuthNetworkDts _authNetworkDts;
+  final AuthLocalDts _authLocalDts;
+  final SessionHandler _sessionHandler;
+
+  AuthRepoImpl(this._authNetworkDts, this._authLocalDts, this._sessionHandler);
+
+  @override
+  Future<RepoResult<UserEntity>> login(LoginFormEntity loginForm) async {
+    final user = await _authNetworkDts.login(loginForm);
+    await _authLocalDts.saveUser(user);
+    await _sessionHandler.setSession(SessionMocked(userId: user.id));
+    return RepoResult(data: user);
+  }
+
+  @override
+  Future<RepoResult<Nothing>> logout() async {
+    await _authLocalDts.deleteUser();
+    await _sessionHandler.clearSession();
+    return RepoResult(data: Nothing());
+  }
+
+  @override
+  Future<RepoResult<UserEntity>> register(
+    RegisterFormEntity registerForm,
+  ) async {
+    final user = await _authNetworkDts.register(registerForm);
+    await _authLocalDts.saveUser(user);
+    return RepoResult(data: user);
+  }
+}
