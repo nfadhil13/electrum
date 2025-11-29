c6585f4546c0b5cf562ee1d6fcd3c0ef5d0fa9a3
Naufal Fadhil <naufalfadhil12344@gmail.com>
2025-11-29 17:29:42 +0700

    [Feature-Auth] Create Datasource

---
+++ lib/features/auth/data/datasources/auth_local_dts.dart
@@ -0,0 +1,7 @@
+import 'package:electrum/features/auth/domain/entities/user.dart';
+
+abstract class AuthLocalDts {
+  Future<void> saveUser(UserEntity user);
+  Future<UserEntity?> getUser();
+  Future<void> deleteUser();
+}
---
+++ lib/features/auth/data/datasources/remote/mocked/auth_mocked_network_dts.dart
@@ -0,0 +1,74 @@
+import 'package:electrum/core/env/environment.dart';
+import 'package:electrum/core/types/exception/base.dart';
+import 'package:electrum/features/auth/data/datasources/auth_network_dts.dart';
+import 'package:electrum/features/auth/domain/entities/login_form.dart';
+import 'package:electrum/features/auth/domain/entities/register_form.dart';
+import 'package:electrum/features/auth/domain/entities/user.dart';
+import 'package:injectable/injectable.dart';
+
+@LazySingleton(as: AuthNetworkDts, env: [AppEnvironment.mocked])
+class AuthMockedNetworkDts implements AuthNetworkDts {
+  final Map<String, UserEntity> users = {};
+
+  AuthMockedNetworkDts() {
+    users['test@test.com'] = UserEntity(
+      id: '1',
+      fullName: 'Test User',
+      email: 'test@test.com',
+      password: 'test',
+      createdAt: DateTime.now(),
+      updatedAt: DateTime.now(),
+    );
+    users['test2@test.com'] = UserEntity(
+      id: '2',
+      fullName: 'Test User 2',
+      email: 'test2@test.com',
+      password: 'test2',
+      createdAt: DateTime.now(),
+      updatedAt: DateTime.now(),
+    );
+  }
+
+  @override
+  Future<UserEntity> login(LoginFormEntity loginForm) async {
+    final user = users[loginForm.email];
+    if (user == null) {
+      throw ApiError(
+        statusCode: 404,
+        message: ApiErrorType.USER_NOT_FOUND.name,
+      );
+    }
+    if (loginForm.password != user.password) {
+      throw ApiError(
+        statusCode: 401,
+        message: ApiErrorType.INVALID_PASSWORD.name,
+      );
+    }
+    return user;
+  }
+
+  @override
+  Future<void> logout() {
+    return Future.value();
+  }
+
+  @override
+  Future<UserEntity> register(RegisterFormEntity registerForm) {
+    if (users.containsKey(registerForm.email)) {
+      throw ApiError(
+        statusCode: 400,
+        message: ApiErrorType.USER_ALREADY_EXISTS.name,
+      );
+    }
+    final user = UserEntity(
+      id: DateTime.now().millisecondsSinceEpoch.toString(),
+      fullName: registerForm.fullName,
+      email: registerForm.email,
+      password: registerForm.password,
+      createdAt: DateTime.now(),
+      updatedAt: DateTime.now(),
+    );
+    users[registerForm.email] = user;
+    return Future.value(user);
+  }
+}
