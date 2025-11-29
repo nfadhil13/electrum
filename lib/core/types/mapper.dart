ac9cab83b5680d16e37e19dd0546b6f9cc34c0a8
Naufal Fadhil <naufalfadhil12344@gmail.com>
2025-11-29 21:44:06 +0700

    [Core-Types] Add Base JSONMapper

--- lib/core/types/mapper.dart
+++ lib/core/types/mapper.dart
@@ -1,9 +1,9 @@
 abstract class FromJSONMapper<T> {
-  T fromJson(Map<String, dynamic> json);
+  T fromJson(dynamic json);
 }
 
 abstract class ToJSONMapper<T> {
-  Map<String, dynamic> toJson(T object);
+  dynamic toJson(T object);
 }
 
 abstract class JSONMapper<T> implements FromJSONMapper<T>, ToJSONMapper<T> {}
