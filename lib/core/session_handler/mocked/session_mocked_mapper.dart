15ec1a7984917e0013844caee9858f48aaa3eb96
Naufal Fadhil <naufalfadhil12344@gmail.com>
2025-11-29 17:27:02 +0700

    [Core-Session] Create Abstract Session Handler

---
+++ lib/core/session_handler/mocked/session_mocked_mapper.dart
@@ -0,0 +1,16 @@
+import 'package:electrum/core/session_handler/session.dart';
+import 'package:electrum/core/types/mapper.dart';
+
+class SessionMockedMapper implements JSONMapper<SessionMocked> {
+  static const String userIdKey = 'userId';
+
+  @override
+  SessionMocked fromJson(Map<String, dynamic> json) {
+    return SessionMocked(userId: json[userIdKey] as String);
+  }
+
+  @override
+  Map<String, dynamic> toJson(SessionMocked object) {
+    return {userIdKey: object.userId};
+  }
+}
---
+++ lib/core/session_handler/session.dart
@@ -0,0 +1,21 @@
+import 'package:equatable/equatable.dart';
+
+sealed class Session extends Equatable {
+  const Session();
+}
+
+class SessionMocked extends Session {
+  final String userId;
+  final Map<String, dynamic>? metadata;
+
+  const SessionMocked({required this.userId, this.metadata});
+
+  @override
+  List<Object?> get props => [userId, metadata];
+}
+
+abstract class SessionHandler {
+  Future<Session?> getSession();
+  Future<void> setSession(Session session);
+  Future<void> clearSession();
+}
