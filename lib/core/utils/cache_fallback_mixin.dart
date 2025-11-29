7550678fb45582adeb5cd8b65bc42ab94acaf20c
Naufal Fadhil <naufalfadhil12344@gmail.com>
2025-11-29 21:44:28 +0700

    [Core-Utils] - Add Cache Fallback Mixin

---
+++ lib/core/utils/cache_fallback_mixin.dart
@@ -0,0 +1,24 @@
+import 'package:electrum/core/types/repo_result.dart';
+
+mixin CacheFallbackMixin {
+  Future<RepoResult<T>> withCacheFallback<T>({
+    required Future<T> Function() networkCall,
+    required Future<T?> Function() getCache,
+    Future<void> Function(T)? saveCache,
+  }) async {
+    try {
+      final data = await networkCall();
+      if (saveCache != null) {
+        await saveCache(data);
+      }
+      return RepoResult(data: data);
+    } catch (e) {
+      final cached = await getCache();
+      if (cached != null) {
+        return RepoResult(data: cached);
+      }
+      rethrow;
+    }
+  }
+}
+
