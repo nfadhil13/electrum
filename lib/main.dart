c431d5002df0ed8e4cc1b2a4bf3fb54d953011b8
Naufal Fadhil <naufalfadhil12344@gmail.com>
2025-11-30 15:24:51 +0700

    [MAIN] Add Multiple Setup to Main (DI, Localization, Styling)

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
+import 'package:electrum/core/env/environment.dart';
+import 'package:electrum/core/localization/app_localizations.dart';
 import 'package:electrum/core/router/router.dart';
+import 'package:electrum/core/service_locator/service_locator.dart';
+import 'package:electrum/core/ui/styles/style.dart';
 import 'package:flutter/material.dart';
 
 void main() {
+  configureDependencies(AppEnvironment.mocked);
   runApp(const MyApp());
 }
 
@@ -13,6 +18,10 @@ class MyApp extends StatelessWidget {
     return MaterialApp.router(
       title: 'Flutter Demo',
       routerConfig: AppRouter.router,
+      localizationsDelegates: AppLocalizations.localizationsDelegates,
+      supportedLocales: AppLocalizations.supportedLocales,
+      builder: (context, child) =>
+          ElectrumStyleProvider(child: child ?? const SizedBox()),
     );
   }
 }
